63c94d289130f2333016ba1e5bfe38652f984673
Yevgeny Menaker <jeka.menaker@gmail.com>
2020-12-01 21:37:09 -0800

    update run_capture to accept device name

--- app/run_capture.py
+++ app/run_capture.py
@@ -1,21 +1,35 @@
 import time
 import os
+import sys
+import signal
 
 from quantotto.capture.manager import CaptureManager
 
 QUANTOTTO_HOME = os.getenv("QUANTOTTO_HOME")
 
+cap_mgr = None
+
+def exit_gracefully(signum, frame):
+   cap_mgr.stop_all()
+
+signal.signal(signal.SIGINT, exit_gracefully)
+signal.signal(signal.SIGTERM, exit_gracefully)
+
 if __name__ == "__main__":
-    cap_mgr = CaptureManager(f"{QUANTOTTO_HOME}/capture_config.yaml")
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <device name>")
+    device_name = sys.argv[1]
+    cap_mgr = CaptureManager(
+        f"{QUANTOTTO_HOME}/capture_config.yaml",
+        device_name
+    )
     while True:
         try:
-            cap_mgr.run_all()
-        except KeyboardInterrupt:
-            print("Capture quitting")
-            cap_mgr.stop_all()
+            cap_mgr.run()
             break
         except Exception as e:
             print(f"Exception: {e}")
-            cap_mgr.stop_all()
+            cap_mgr.stop()
             time.sleep(5.0)
 
+    print("run_capture.py is done")
